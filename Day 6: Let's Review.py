92c7254aa36f34f41ffcb690e955acc01bc4fb3f
Gilbert <gilbertduenas@users.noreply.github.com>
2020-10-17 11:18:25 +0900

    Create Day 6: Let's Review.py

---
+++ Day 6: Let's Review.py
@@ -0,0 +1,21 @@
+# https://www.hackerrank.com/challenges/30-review-loop/problem
+
+n = int(input())
+
+def string_loop(s):
+    e = []
+    o = []
+
+    for i in range(len(s)):
+        if i%2 == 0:
+            e.append(s[i])
+        else:
+            o.append(s[i])
+
+    E = ''.join(e)
+    O = ''.join(o)
+
+    return f'{E} {O}'
+
+for i in range(n):
+    print(string_loop(input()))
