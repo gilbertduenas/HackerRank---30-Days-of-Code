e17d95fe99d35cb37796355f11610d8a01880111
Gilbert <gilbertduenas@users.noreply.github.com>
2020-10-18 17:41:40 +0900

    Update Day 8: Dictionaries and Maps.py

--- Day 8: Dictionaries and Maps.py
+++ Day 8: Dictionaries and Maps.py
@@ -1,1 +1,22 @@
 # https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
+
+n = int(input())
+
+d = {}
+
+for i in range(n):
+    k, v = map(str, input().split())
+
+    d[k] = v
+
+for j in range(n):
+    try:
+        s = input()
+
+        if s in d.keys():
+            print(f'{s}={d[s]}')
+        else:
+            print('Not found')
+            
+    except:
+        break 
