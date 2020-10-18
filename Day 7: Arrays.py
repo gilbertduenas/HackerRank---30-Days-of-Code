4f3ba39db4272228ebea2455a9adc1c1c070dd8d
Gilbert <gilbertduenas@users.noreply.github.com>
2020-10-18 10:17:38 +0900

    Update Day 7: Arrays.py
    Updated the wrong file.

--- Day 7: Arrays.py
+++ Day 7: Arrays.py
@@ -1,25 +1,16 @@
 # https://www.hackerrank.com/challenges/30-arrays/problem
 
-n = int(input())
+#!/bin/python3
 
-def string_loop(s):
-    e = []
-    o = []
+import math
+import os
+import random
+import re
+import sys
 
-    for i in range(len(s)):
-        if i%2 == 0:
-            e.append(s[i])
-        else:
-            o.append(s[i])
+if __name__ == '__main__':
+    n = input()
 
-    E = ''.join(e)
-    O = ''.join(o)
-
-    return f'{E} {O}'
-
-# Or I could do this to replace lines 6 to 18.
-#     for i in range(n):
-#         return f'{s[::2]} {s[1::2]}'
-
-for i in range(n):
-    print(string_loop(input()))
+    arr = list(map(str, input().rstrip().split()))
+    
+    print(' '.join(reversed(arr)))
