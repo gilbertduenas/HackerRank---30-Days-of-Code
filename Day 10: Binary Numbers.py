f0e2915f94519781ac8e8aaf1604e03819498d8b
Gilbert <gilbertduenas@users.noreply.github.com>
2020-10-20 13:54:19 +0900

    Update Day 10: Binary Numbers.py

--- Day 10: Binary Numbers.py
+++ Day 10: Binary Numbers.py
@@ -1,2 +1,13 @@
 # https://www.hackerrank.com/challenges/30-binary-numbers/problem
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+n = int(input())
+b = bin(n)[2:].split('0')
+print(len(max(b)))
