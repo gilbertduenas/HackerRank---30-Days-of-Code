bbdbad2fac552b285bad0909c2007d0a4ac936da
Gilbert <gilbertduenas@users.noreply.github.com>
2020-10-25 19:52:00 +0900

    Update Day 14: Scope.py

--- Day 14: Scope.py
+++ Day 14: Scope.py
@@ -1,2 +1,21 @@
 # https://www.hackerrank.com/challenges/30-scope/problem
 
+Difference:
+    def __init__(self, a):
+        self.__elements = a
+
+    def computeDifference(self):
+        max_ele = max(self.__elements)
+        min_ele = min(self.__elements)
+
+        self.maximumDifference = abs(max_ele - min_ele)
+    
+# End of Difference class
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
