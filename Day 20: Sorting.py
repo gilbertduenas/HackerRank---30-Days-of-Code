4713a573112781f759dc91dbee6e4e34fbafd979
Gilbert <gilbertduenas@users.noreply.github.com>
2020-11-01 18:50:14 +0900

    Update Day 20: Sorting.py

--- Day 20: Sorting.py
+++ Day 20: Sorting.py
@@ -1,2 +1,19 @@
 # https://www.hackerrank.com/challenges/30-sorting/problem
 
+import sys
+
+n = int(raw_input().strip())
+a = map(int, raw_input().strip().split(' '))
+
+def bubble(arr):
+    count_swap = 0
+    for num in range(len(arr)-1, 0, -1):
+        for i in range(num):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                count_swap +=1
+    return count_swap
+
+print "Array is sorted in {} swaps.".format(bubble(a))
+print "First Element: {}".format(a[0])
+print "Last Element: {}".format(a[-1])
